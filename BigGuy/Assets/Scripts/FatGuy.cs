f3d84c5c76d6ea96a713f78be73b319ef5f4805d
PontanPOP <pontan4@live.se>
2018-12-06 15:41:13 +0100

    Re-Dokne

--- BigGuy/Assets/Scripts/FatGuy.cs
+++ BigGuy/Assets/Scripts/FatGuy.cs
@@ -115,6 +115,7 @@ public class FatGuy : MonoBehaviour
     {
         if (col.gameObject.tag == "Food")
         {
+            Destroy(col.gameObject);
             health += 5;
         }
     }
---
+++ BigGuy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text points;
+    [SerializeField] int scorePerSecond;
+
+    int totalScore = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        points.text = totalScore.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
