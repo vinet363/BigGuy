b7a8e0f32e053406d6ff2167041c23f951ef8d56
Unknown <s.sweden98@gmail.com>
2018-12-18 11:50:15 +0100

    menu > game > gameover > menu

---
+++ BigGuy/Assets/Scripts/StartButtonMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StartButtonMenu : MonoBehaviour
+{
+    [SerializeField] GameObject startButton;
+    EventSystem m_EventSystem;
+
+    private void OnEnable()
+    {
+        m_EventSystem = EventSystem.current;
+        StartCoroutine("ButtonSpawn");
+    }
+
+    IEnumerator ButtonSpawn()
+    {
+        yield return new WaitForSeconds(0.01f);
+        m_EventSystem.SetSelectedGameObject(startButton);
+    }
+}
